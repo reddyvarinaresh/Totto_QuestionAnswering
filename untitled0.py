764d64021874873923939f1465a72236cecdbb77
Reddyvari Naresh <39510382+reddyvarinaresh@users.noreply.github.com>
2022-06-08 18:53:47 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 11:18:18 2021
+
+@author: reddyvari.naresh
+"""
+
+import os
+os.popen('C:\Users\reddyvari.naresh\OneDrive - Incedo Technology Solutions Ltd\Desktop\NLG\Totto\totto_eval.sh' --prediction_path 'C:\Users\reddyvari.naresh\OneDrive - Incedo Technology Solutions Ltd\Desktop\NLG\Totto\sample\output_sample.txt' --target_path 'C:\Users\reddyvari.naresh\OneDrive - Incedo Technology Solutions Ltd\Desktop\NLG\Totto\sample\dev_sample.jsonl')
